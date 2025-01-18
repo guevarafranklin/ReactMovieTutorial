1a6d24c70e154a9a035b1b5c577994fa4369aca8
guevarafranklin <tecguevara@gmail.com>
2025-01-18 11:27:39 -0600

    finished adding the CSS changes to Favorites part.

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,20 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites(){
+    const {favorites} = useMovieContext();
+
+    if (favorites) { 
+        return (
+        <div className="favorites">
+            <h2>Your Favorite Movies</h2>
+            <div className="movies-grid">
+                {favorites.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
+        );
+    }
     return <div className="favorite-page">
         <h2>Favorite Movies</h2>
         <div className="favorites-empty">
