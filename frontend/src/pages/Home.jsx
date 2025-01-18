a944b79ac0292e869d02f1197a51470f86b23d56
guevarafranklin <tecguevara@gmail.com>
2025-01-18 10:00:04 -0600

    added the use effect and the created the API service with the variables.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {getPopularMovies, searchPopularMovies} from "../services/api.js";
 import "../css/Home.css";
 
 function Home (){
 const [searchQuery, setSearchQuery] = useState('');
+const [movies, setMovies] = useState([]);
+useEffect(() => {
+    getPopularMovies().then((movies) => setMovies(movies));
+}
 
-    const movies  = [
-        {id: 1, title: 'The Dark Knight', release_date: '2008', url: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg'},
-        {id: 2, title: 'Inception', release_date: '2010', url: 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg'},
-        {id: 3, title: 'Interstellar', release_date: '2014', url: 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg'},
-        {id: 4, title: 'Dunkirk', release_date: '2017', url: 'https://image.tmdb.org/t/p/w500/ebSnODDg9lbsMIaWg2uAbjn7TO5.jpg'},
-        {id: 5, title: 'Tenet', release_date: '2020', url: 'https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg'},
-        {id: 6, title: 'The pick of destiny', release_date: '2006', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzj3fTX2ScB0c9hLeWncByY3Q4mcHnFBWOakXhb10KUQZ9miSRXwys4SlCuvG8plOp91bVAA'}
-        ];
+
         const handleSearch = (e) => {
             e.preventDefault();
             alert(searchQuery);
---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "7bf40b8083ced51bf2fa9b13ce224329";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchPopularMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
